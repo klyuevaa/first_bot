c2afb1a08b5923558ec1c6c341dfe8aecd605f9f
klyuevaa <python.klyuev@gmail.com>
2021-11-21 00:25:08 +0300

    first bot

---
+++ bot.py
@@ -0,0 +1,31 @@
+import logging
+import settings
+from telegram.ext import Updater, CommandHandler,  MessageHandler, Filters
+
+logging.basicConfig(filename='bot.log', level=logging.INFO)
+
+def greet_user(update, context):
+    print('Вызван /start')
+    update.message.reply_text('Привет, пользователь! Ты вызвал команду /start')
+
+def talk_to_me(update, context):
+    user_text = update.message.text 
+    print(user_text)
+    update.message.reply_text(user_text)
+
+def main():
+    # Создаем бота и передаем ему ключ для авторизации на серверах Telegram
+    mybot = Updater(settings.API_KEY, use_context=True)
+
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler("start", greet_user))
+    logging.info("Бот стартовал")
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+
+    # Командуем боту начать ходить в Telegram за сообщениями
+    mybot.start_polling()
+    # Запускаем бота, он будет работать, пока мы его не остановим принудительно
+    mybot.idle()
+
+    if __name__ == "__main__":
+        main()
